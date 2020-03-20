e4dc82fc05bfaaea1917af568796b4b64388d10b
Borja Cabeza <borjacabeza@msn.com>
2020-03-20 13:31:17 +0100

    Primera Clases 20-Mar-2020

---
+++ WebAppRefuerzo1Ejercicio/api/data.ashx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace WebAppRefuerzo1Ejercicio.api
+{
+    /// <summary>
+    /// Descripción breve de data
+    /// </summary>
+    public class data : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            try
+            {
+                string id = context.Request.Params["id"];
+                string key = (context.Request.Headers.AllKeys.Contains("API-Key") ? context.Request.Headers.Get("API-Key") : "");
+
+                if (id.ToLower() == "all" && key.ToLower() == "ce4def9a-beb1-4225-82fb-897109f5d778")
+                {
+                    string json = File.ReadAllText(context.Request.PhysicalApplicationPath + "api\\data.json");
+                    context.Response.ContentType = "text/plain";
+                    context.Response.Write(json);
+                }
+                else
+                {
+                    context.Response.ContentType = "text/plain";
+                    context.Response.Write("No Autorizado o Petición Erronea.");
+                }
+            }
+            catch (Exception e)
+            {
+                context.Response.ContentType = "text/plain";
+                context.Response.Write("Error: " + e.Message);
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
